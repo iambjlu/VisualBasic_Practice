df21be980819b1effffafdf7591a07458dc50a68
蘿蔔 <55518831+iambjlu@users.noreply.github.com>
2022-02-15 13:57:19 +0800

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btn_01_Click(sender As Object, e As EventArgs) Handles btn_01.Click
+        Dim m0, m1, m2, m3, m4
+        FileOpen(1, "C:\丙設資料\1060301.SM", OpenMode.Input)
+        Input(1, m0)
+        FileClose(1)
+        m1 = m0
+        m2 = m1 / 10
+        m3 = m1 Mod 10
+        m4 = m4 & m3
+
+    End Sub
+End Class
