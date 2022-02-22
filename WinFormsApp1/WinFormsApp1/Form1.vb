dca53021bfb071e22b0e6f1cbc0615866fb93e3c
Administrator <Administrator@WIN11DEVENV>
2022-02-22 11:22:11 +0800

    commit

--- WinFormsApp1/WinFormsApp1/Form1.vb
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -4,10 +4,24 @@
         FileOpen(1, "C:\丙設資料\1060301.SM", OpenMode.Input)
         Input(1, m0)
         FileClose(1)
+        m0 = 1234554321
         m1 = m0
-        m2 = m1 / 10
-        m3 = m1 Mod 10
-        m4 = m4 & m3
+        For i = 1 To 9
+            m2 = m1 \ 10
+            m3 = m1 Mod 10
+            m4 = m4 & m3
+            If m2 = 0 Then Exit For
+            m1 = m2
+        Next i
+
+        If m0 = m4 Then
+            txt_01.Text = "第一題結果 : " & m0 & " is a palindrome."
+        Else
+            txt_01.Text = "第一題結果 : " & m0 & " is not a palindrome."
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
