18eae9891d516ab77cd1a40b9ad768ab63bdaf59
Administrator <Administrator@WIN11DEVENV>
2022-03-15 11:12:10 +0800

    全部認可

---
+++ 1060304/WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btn = New System.Windows.Forms.Button()
+        Me.txt = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btn
+        '
+        Me.btn.Location = New System.Drawing.Point(286, 39)
+        Me.btn.Name = "btn"
+        Me.btn.Size = New System.Drawing.Size(134, 60)
+        Me.btn.TabIndex = 0
+        Me.btn.Text = "第四題"
+        Me.btn.UseVisualStyleBackColor = True
+        '
+        'txt
+        '
+        Me.txt.Location = New System.Drawing.Point(208, 122)
+        Me.txt.Multiline = True
+        Me.txt.Name = "txt"
+        Me.txt.Size = New System.Drawing.Size(311, 193)
+        Me.txt.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txt)
+        Me.Controls.Add(Me.btn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn As Button
+    Friend WithEvents txt As TextBox
+End Class
---
+++ 1060304/WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub btn_Click(sender As Object, e As EventArgs) Handles btn.Click
+        Dim h(3), w(3), bmi(3), s_bmi
+        FileOpen(1, "C:\丙設資料\1060304.SM", OpenMode.Input)
+
+        For i = 1 To 3
+            Input(1, h(i)) : Input(1, w(i))
+            bmi(i) = w(i) / (h(i) / 100) ^ 2
+
+        Next
+        FileClose(1)
+
+        s_bmi = bmi(1)
+        For i = 1 To 3
+            If bmi(i) < s_bmi Then
+                s_bmi = bmi(i)
+            End If
+
+        Next
+        Dim s_bmi45 As Integer = s_bmi
+
+        If s_bmi45 >= 20 And s_bmi45 <= 25 Then
+            txt.Text = "第四題結果：最小BMI值=" & s_bmi45 & "，正常"
+        Else
+            txt.Text = "第四題結果：最小BMI值=" & s_bmi45 & "，不正常"
+        End If
+
+    End Sub
+End Class
