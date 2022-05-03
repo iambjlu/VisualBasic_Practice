a6128e5917493e1b1be8a1ef9002607968be34eb
Administrator <Administrator@WIN11DEVENV>
2022-05-03 11:53:15 +0800

    全部認可

---
+++ 1060307/1060307/Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(43, 35)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(561, 193)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(355, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 15)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Label1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(32, 152)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 15)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Label1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(355, 152)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 15)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Label1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(407, 22)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 25)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(84, 22)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 25)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(84, 149)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 25)
+        Me.TextBox3.TabIndex = 1
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(407, 152)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 25)
+        Me.TextBox4.TabIndex = 1
+        '
+        'dgv
+        '
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Location = New System.Drawing.Point(43, 258)
+        Me.dgv.Name = "dgv"
+        Me.dgv.RowHeadersWidth = 51
+        Me.dgv.RowTemplate.Height = 27
+        Me.dgv.Size = New System.Drawing.Size(561, 327)
+        Me.dgv.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(643, 640)
+        Me.Controls.Add(Me.dgv)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents dgv As DataGridView
+End Class
---
+++ 1060307/1060307/Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+    Dim suit(4) As String
+    Dim d(100)
+    Dim card As New ArrayList
+    Dim gno As Integer
+    Dim rno As Integer
+    Dim pn, pf, pn1 As New ArrayList
+    Dim bn, bf, bn1 As New ArrayList
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call rdata()
+        Call sp1()
+        Call sp2()
+        Call sp3()
+        Call wdata()
+    End Sub
+
+    Sub rdata()
+        FileOpen(1, "C:\丙設資料\1060307.SM", OpenMode.Input)
+        Input(1, gno)
+        rno = 0
+        Do While EOF(1)
+            rno = rno + 1
+            Input(1, d(rno))
+        Loop
+        FileClose(1)
+
+    End Sub
+
+    Sub sp1()
+        Dim ba1() As Byte = {226, 153, 160}
+        Dim ba2() As Byte = {226, 153, 165}
+        Dim ba3() As Byte = {226, 153, 166}
+        Dim ba4() As Byte = {226, 153, 163}
+        suit(0) = System.Text.Encoding.UTF8.GetString(ba1)
+        suit(1) = System.Text.Encoding.UTF8.GetString(ba2)
+        suit(2) = System.Text.Encoding.UTF8.GetString(ba3)
+        suit(3) = System.Text.Encoding.UTF8.GetString(ba4)
+    End Sub
+
+    Sub sp2()
+        For i = 1 To rno
+            Dim t = Int(Val(d(i) * 52))
+            If card.IndexOf(t) = -1 Then card.Add(t)
+        Next
+    End Sub
+
+    Sub sp3()
+        For i = 0 To gno * 2 - 1
+            Dim f = card(i) \ 13
+            Dim n = card(i)
+            Dim disp = {"*", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+            Dim n1 = disp(n)
+            If n = 1 Then n = n + 13
+            If i Mod 2 = 0 Then
+                pn.Add(n) : pf.Add(f) : pn1.Add(n1)
+            Else
+                bn.Add(n) : bf.Add(f) : bn1.Add(n1)
+            End If
+        Next
+    End Sub
+
+    Sub wdata()
+
+    End Sub
+End Class
